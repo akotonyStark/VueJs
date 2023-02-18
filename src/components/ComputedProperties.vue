b6995c4eaaf23d3e985408c44a665fc0b749dc7d
akotonystark <alarbiampofo@gmail.com>
2023-02-18 06:40:59 +0000

    computed values are cashed

--- src/components/ComputedProperties.vue
+++ src/components/ComputedProperties.vue
@@ -3,7 +3,13 @@
         <h2>First Name: {{firstName}} LastName: {{lastName}} </h2>
         <h2>Computed FullName: {{fullName}} </h2>
         <button @click="items.push({id:3, name:'Airpods', price: 179})">Add Item to Cart</button>
-        <h2>Total: {{totalPrice}}</h2>
+        <h2>Computed Total: {{totalPrice}}</h2>
+        <h2>Method Total: {{getTotal()}}</h2>
+        
+        <template v-for="item in items">
+            <h3 v-if="item.price > 1000" :key="item.id">{{item.name}}</h3>
+        </template>
+        <h3 v-for="item in inexpensiveItems" :key="item.id">{{item.name}}</h3>
     </div>
 </template>
 <script>
@@ -22,14 +28,21 @@ export default {
                 {
                     id:2,
                     name: 'MacBook Air M1',
-                    price: 999
+                    price: 1099
+                },
+                {
+                    id:3,
+                    name: 'MacBook Air M2',
+                    price: 1299
                 }
             ]
         }
     },
 
     methods:{
-
+        getTotal(){
+            return this.items.reduce((total, item) => total = total + item.price, 0)
+        }
     },
     computed: {
         fullName(){
@@ -37,6 +50,9 @@ export default {
         },
         totalPrice(){
             return this.items.reduce((total, item) => total = total + item.price, 0)
+        },
+        inexpensiveItems(){
+            return this.items.filter(item => item.price < 1000) 
         }
 
     }
