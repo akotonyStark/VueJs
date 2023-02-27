03771e324f321a7e8dfafc4b6fcbcfe66666bb40
akotonystark <alarbiampofo@gmail.com>
2023-02-27 02:27:00 +0000

    named slots

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,14 @@
   <Card>
     <h3>Some content</h3>
   </Card>
+  <Card>
+    <img src="https://picsum.photos/200">
+  </Card>
+  <Card>
+    <template v-slot:header><h2>Header</h2></template>
+    <template v-slot:default><img src="https://picsum.photos/200"></template>
+    <template v-slot:footer><button>View Details</button></template>
+  </Card>
 
   <h2>Logged in as {{ username }}</h2>
   <pre>These are components with props</pre>
